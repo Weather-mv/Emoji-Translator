93d3a5ac297d072c015e55af096caed07af254ca
Weather-mv <mousam07999@gmail.com>
2025-07-01 17:52:22 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const emojiDictionary={
+  happy: "😄",
+  love: "❤️",
+  angry: "😡",
+  hello: "👋",
+  fire: "🔥",
+  sad: "😢",
+  cool: "😎",
+  wow: "😲",
+  sleep: "😴",
+  ok: "👌"
+}
 
 const App = () => {
+  
+  const [input, setInput] = useState("")
+  const [translated, setTranslated] = useState("")
+
+  const handleChange=(e)=>{
+    const text = e.target.value.toLowerCase();
+    setInput(text);
+    
+    const words=text.split("");
+    const result=words.map((word)=>emojiDictionary[word] || word).join(" ");
+    setTranslated(result);
+  }
   return (
     <div className='App'>
     <h1>😊 Emoji Translator</h1>
+    <input className='input' type="text" placeholder='Type : Happy, Love, Fire, Angry' value={input} onChange={handleChange}/>
+    <p>{translated}</p>
     </div>
   )
 }
